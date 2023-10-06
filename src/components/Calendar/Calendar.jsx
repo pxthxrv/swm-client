b3405ce46492abab4fc17446f9262544c6c60f39
Eric Lo <eric.lo.ca@outlook.com>
2023-10-06 07:00:51 -0700

    added clicking features

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -5,11 +5,21 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
 const Calendar = ({ view }) => {
+  const dateEventHandler = (info) => {
+    alert("Clicked on: " + info.dateStr);
+    alert("Coordinates: " + info.jsEvent.pageX + "," + info.jsEvent.pageY);
+    alert("Current view: " + info.view.type);
+    // change the day's background color just for fun
+    info.dayEl.style.backgroundColor = "red";
+  };
+
   return (
     <div>
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView={view}
+        selectable={true}
+        dateClick={dateEventHandler}
       />
     </div>
   );
