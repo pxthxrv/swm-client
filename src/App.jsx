789264f8a0bbf250dfffadfb3eec57a3ffad0507
Adam <Adamz1865@gmail.com>
2023-10-06 11:45:19 -0400

    Added simple routing to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.scss'
 
 function App() {
@@ -6,13 +7,18 @@ function App() {
 
   return (
     <>
-    <h1>This is a h1 tag</h1>
-    <h2>This is a h2 tag</h2>
-    <h3>This is a h3 tag</h3>
-    <h4>This is a h4</h4>
-    <h5>This is a h5</h5>
-    <p>This is a p tag</p>
-    <a>This is an a tag</a>
+      <h1>This is a h1 tag</h1>
+      <h2>This is a h2 tag</h2>
+      <h3>This is a h3 tag</h3>
+      <h4>This is a h4</h4>
+      <h5>This is a h5</h5>
+      <p>This is a p tag</p>
+      <a>This is an a tag</a>
+      <Router>
+        <Route path='/Comment'>
+          <Comment />
+        </Route>
+      </Router>
     </>
   )
 }
