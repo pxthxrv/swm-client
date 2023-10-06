44d3facfb77b0ad58b7c45a0249708798a3bc335
Patrick Harvey <patharvey@protonmail.com>
2023-10-06 13:46:56 -0400

    finish header

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
-import './App.scss'
-import Footer from './components/Footer/Footer'
+import { useState } from "react";
+import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    <h1>This is a h1 tag</h1>
-    <h2>This is a h2 tag</h2>
-    <h3>This is a h3 tag</h3>
-    <h4>This is a h4</h4>
-    <h5>This is a h5</h5>
-    <p>This is a p tag</p>
-    <a>This is an a tag</a>
-    <Footer />
-    </>
-  )
+    <div className="iphone-container">
+      <Header />
+      <h1>Key Insights</h1>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
