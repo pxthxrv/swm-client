3858c34ee15fb995e5171ae2a0abb38d75c9e460
Patrick Harvey <patharvey@protonmail.com>
2023-10-06 13:23:01 -0400

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,36 @@
 import "./Footer.scss";
+import homeIcon from "../../assets/icons/home.svg";
+import bankIcon from "../../assets/icons/bank.svg";
+import portfolioIcon from "../../assets/icons/portfolio.svg";
+import profileIcon from "../../assets/icons/profile.svg";
+import homeIndicator from "../../assets/icons/home-indicator.svg"
 
 export default function Footer() {
     return (
         <>
-            <h1>Advisory Team</h1>
+            <div className="footer">
+                <div className="footer__row">
+                    <div className="ftr">
+                        <img className="ftr__icon" src={homeIcon} alt=""/>
+                        <p className="ftr__text">Home</p>
+                    </div>
+                    <div className="ftr">
+                        <img className="ftr__icon" src={bankIcon} alt=""/>
+                        <p className="ftr__text">Bank</p>
+                    </div>
+                    <div className="ftr">
+                        <img className="ftr__icon--portfolio" src={portfolioIcon} alt=""/>
+                        <p className="ftr__text">Portfolio</p>
+                    </div>
+                    <div className="ftr">
+                        <img className="ftr__icon" src={profileIcon} alt=""/>
+                        <p className="ftr__text">Profile</p>
+                    </div>
+                </div>
+                <div className="footer__col">
+                    <img className="home-indicator" src={homeIndicator}/>
+                </div>
+            </div>
         </>
     )
 
